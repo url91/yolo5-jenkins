039c432c2a9bf31e7a79f9359899076ca75a33c5
urits91 <urits91@gmai.com>
2023-03-23 19:48:23 +0200

    Build

---
+++ infra/main.tf
@@ -0,0 +1,117 @@
+/*
+ The terraform {} block contains Terraform settings, including the required providers Terraform will use to provision infrastructure.
+ Terraform installs providers from the Terraform Registry by default.
+ In this example configuration, the aws provider's source is defined as hashicorp/aws,
+*/
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+
+/*
+ The provider block configures the specified provider, in this case aws.
+ You can use multiple provider blocks in your Terraform configuration to manage resources from different providers.
+*/
+provider "aws" {
+  #profile = aws profile name
+  region  = "eu-north-1"
+}
+
+
+/*
+ Use resource blocks to define components of your infrastructure.
+ A resource might be a physical or virtual component such as an EC2 instance.
+ A resource block declares a resource of a given type ("aws_instance") with a given local name ("app_server").
+ The name is used to refer to this resource from elsewhere in the same Terraform module, but has no significance outside that module's scope.
+ The resource type and name together serve as an identifier for a given resource and so must be unique within a module.
+
+ For full description of this resource: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/instance
+*/
+resource "aws_instance" "app_server" {
+  ami  = data.aws_ami.amazon_linux_ami.id
+  instance_type = var.env == "prod" ? "t3.micro" : "t3.nano"
+  vpc_security_group_ids = [aws_security_group.sg_web.id]
+  key_name = "url-key"
+  subnet_id = module.app_vpc.public_subnets[0]
+  depends_on = [aws_s3_bucket.data_bucket]
+
+  tags = {
+    Name = "url-terraform-${var.env}"
+    Terraform = "true"
+  }
+}
+
+
+
+resource "aws_s3_bucket" "data_bucket" {
+  bucket = "url-s3-bucket"
+
+  tags = {
+    Name        = "${var.resource_alias}-bucket"
+    Env         = var.env
+    Terraform   = true
+  }
+}
+
+resource "aws_security_group" "sg_web" {
+  name = "${var.resource_alias}-${var.env}-sg"
+  vpc_id = module.app_vpc.vpc_id
+  ingress {
+      from_port   = "8080"
+      to_port     = "8080"
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+      from_port   = "22"
+      to_port     = "22"
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Env         = var.env
+    Terraform   = true
+  }
+}
+
+data "aws_ami" "amazon_linux_ami" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
+data "aws_availability_zones" "available_azs" {
+  state = "available"
+}
+
+module "app_vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "3.14.0"
+
+  name = "${var.resource_alias}-vpc"
+  cidr = var.vpc_cidr
+
+  azs             = data.aws_availability_zones.available_azs.zone_ids
+  private_subnets = var.vpc_private_subnets
+  public_subnets  = var.vpc_public_subnets
+
+  enable_nat_gateway = false
+
+  tags = {
+    Name        = "${var.resource_alias}-vpc"
+    Env         = var.env
+    Terraform   = true
+  }
+}
---
+++ infra/vpc-vars.tf
@@ -0,0 +1,17 @@
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "vpc_private_subnets" {
+  description = "Private subnets for VPC"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "vpc_public_subnets" {
+  description = "Public subnets for VPC"
+  type        = list(string)
+  default     = ["10.0.3.0/24","10.0.4.0/24"]
+}
